961b837d8ad5b6683952edb1b8c086ca52a50e36
sunghan <jsh000311@gmail.com>
2024-09-22 15:37:36 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Home from './components/Home';
+import AboutMe from './components/AboutMe';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+import './App.css';
 
 function App() {
   return (
-    <div className="app-container">
-      <Sidebar />
-      <Home />
-    </div>
+    <Router>
+      <div className="app-container">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,22 +1,35 @@
-// src/components/Sidebar.js
 import React from 'react';
+import { Typography} from '@mui/material';
+import { NavLink } from 'react-router-dom';
 import './Sidebar.css';
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <div className="profile-section">
-                <img src="https://via.placeholder.com/100" alt="Profile" className="profile-pic" />
-                <h2>정성한</h2>
-                <h4>Junior Developer</h4>
-                <div className="social-links">
-                    <a href="#">About Me</a>
-                    <a href="#">Instagram</a>
-                    <a href="#">GitHub</a>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="sidebar">
+      <div className="profile-section">
+        <img src="https://via.placeholder.com/100" alt="Profile" className="profile-pic" />
+        <h2>Jeong Sunghan</h2>                
+        <Typography sx ={{ mb: 2, mt: 2}}>Junior Developer</Typography>                        
+      </div>
+      <nav className="nav-links">
+        <NavLink to="/" className="nav-item" activeClassName="active" exact>
+          Home
+        </NavLink>
+        <NavLink to="/about" className="nav-item" activeClassName="active">
+          About Me
+        </NavLink>
+        <NavLink to="/skills" className="nav-item" activeClassName="active">
+          Skills
+        </NavLink>
+        <NavLink to="/projects" className="nav-item" activeClassName="active">
+          Projects
+        </NavLink>
+        <NavLink to="/contact" className="nav-item" activeClassName="active">
+          Contact
+        </NavLink>
+      </nav>
+    </div>
+  );
 }
 
 export default Sidebar;
